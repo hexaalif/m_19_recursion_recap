738d947023ce7b2249579bb03c382277d0e28971
hexaalif <hexaalif2020@gmail.com>
2023-05-13 18:50:10 +0600

    working code

--- D_Print_Digits_using_Recursion.c
+++ D_Print_Digits_using_Recursion.c
@@ -1,16 +1,30 @@
 #include <stdio.h>
 
+void recur(int n)
+{
+    if (n == 0)
+    {
+        return;
+    }
+    int x = n % 10;
+    recur(n / 10);
+    printf("%d ", x);
+}
+
 int main()
 {
     int n;
     scanf("%d", &n);
-    int t[n];
     for (int i = 0; i < n; i++)
     {
-        scanf("%d", &t[i]);
-        printf(" ");
-        printf("%d ", t[i]);
+        int t;
+        scanf("%d", &t);
+        recur(t);
+        if (t == 0)
+        {
+            printf("0");
+        }
+        printf("\n");
     }
-
     return 0;
 }
