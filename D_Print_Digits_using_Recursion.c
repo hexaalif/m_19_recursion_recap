39bcaba3ebfd15732a1399893b10fcf02e6ab92d
hexaalif <hexaalif2020@gmail.com>
2023-05-13 16:41:48 +0600

    working code

--- D_Print_Digits_using_Recursion.c
+++ D_Print_Digits_using_Recursion.c
@@ -2,6 +2,15 @@
 
 int main()
 {
+    int n;
+    scanf("%d", &n);
+    int t[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &t[i]);
+        printf(" ");
+        printf("%d ", t[i]);
+    }
 
     return 0;
 }
