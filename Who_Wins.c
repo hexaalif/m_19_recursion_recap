762742b9555685b6c569ad7778239f6a377beaf1
hexaalif <hexaalif2020@gmail.com>
2023-05-14 12:35:14 +0600

    worked

---
+++ Who_Wins.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int tiger, pathan, count = 0;
+    for (int i = 1; i < n; i++)
+    {
+        // scanf("%d %d", &tiger[i], &pathan[i]);
+    }
+    return 0;
+}
