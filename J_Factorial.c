2a59e9487a49bfaad91d5826fc65648a1e8cfa05
hexaalif <hexaalif2020@gmail.com>
2023-05-14 10:49:27 +0600

    worked

--- J_Factorial.c
+++ J_Factorial.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
 
+int fac(int n)
+{
+    // base case
+    if (n == 0)
+        return;
+}
+
 int main()
 {
     int n;
     scanf("%d", &n);
-    int sum = 0;
-    int s = 0;
-    for (int i = n; i >= 1; i--)
-    {
-        s = i * (i - 1);
-        sum = s + sum;
-    }
+    // int sum = 0;
+    int sum = fac(n);
+    // int s = 0;
+    // for (int i = n; i >= 1; i--)
+    // {
+    //     s = i * (i - 1);
+    //     sum = s + sum;
+    // }
     printf("%d", sum);
+
     return 0;
 }
