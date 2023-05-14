b8ebdd416f9e6ee3baa0ec18c2242fd8e1b3b05f
hexaalif <hexaalif2020@gmail.com>
2023-05-14 12:59:43 +0600

    worked

--- Who_Wins.c
+++ Who_Wins.c
@@ -4,10 +4,38 @@ int main()
 {
     int n;
     scanf("%d", &n);
-    int tiger, pathan, count = 0;
-    for (int i = 1; i < n; i++)
+    int tiger[n], pathan[n], countT = 0, countP = 0, draw = 0;
+    for (int i = 1; i <= n; i++)
     {
-        // scanf("%d %d", &tiger[i], &pathan[i]);
+        scanf("%d %d", &tiger[i], &pathan[i]);
+        if (tiger[i] == pathan[i])
+        {
+            draw++;
+        }
+        else if (tiger[i] > pathan[i])
+        {
+            countT++;
+        }
+        else
+        {
+            countP++;
+        }
     }
+    if (countT == countP && draw > 0)
+    {
+        printf("Draw");
+    }
+    else if (countT > countP)
+    {
+        printf("Tiger");
+    }
+    else
+    {
+        printf("Pathan");
+    }
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     printf("%d %d\n", tiger[i], pathan[i]);
+    // }
     return 0;
 }
